9f43b5ee330e051d65584980fec8480e8bc52bc3
PORTATIL_MARCO\marco <marco.casati74@gmail.com>
2025-07-23 08:41:50 +0200

    desarollo funcon muestraCarta

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import "./style.css";
 
 document.addEventListener("DOMContentLoaded", () => {
-  muestraPuntuacion(puntuacion);
   eventos();
+  muestraPuntuacion(puntuacion);
 });
 
 let puntuacion = 0;
@@ -12,18 +12,60 @@ const muestraPuntuacion = (puntuacion: number): void => {
   if (puntos) {
     puntos.textContent = puntuacion.toString();
   } else {
-    console.log("Elemento ' puntos' no encontrado");
+    console.log("Elemento 'puntos' no encontrado");
   }
 };
 
 /* ---------------------------------------------FUNCION PARA PEDIR CARTAS ------------------------ */
 
-const dameCarta = (): number => {
-  let numeroCarta = Math.floor(Math.random() * 10) + 1;
+const dameCarta = (): void => {
+  let carta = Math.floor(Math.random() * 10) + 1;
 
-  if (numeroCarta > 7) numeroCarta + 2;
+  if (carta > 7) {
+    carta += 2;
+  }
 
-  return numeroCarta;
+  muestraCarta(carta);
+};
+
+/* ---------------------------------------------FUNCION MOSTRAR CARTA ----------------------------- */
+
+const muestraCarta = (carta: number): void => {
+  const imagen = document.getElementById("imagen-carta") as HTMLImageElement;
+  if (imagen) {
+    switch (carta) {
+      case 1:
+        imagen.src = "src/img/1_as-copas.jpg";
+        break;
+      case 2:
+        imagen.src = "src/img/2_dos-copas.jpg";
+        break;
+      case 3:
+        imagen.src = "src/img/3_tres-copas.jpg";
+        break;
+      case 4:
+        imagen.src = "src/img/4_cuatro-copas.jpg";
+        break;
+      case 5:
+        imagen.src = "src/img/5_cinco-copas.jpg";
+        break;
+      case 6:
+        imagen.src = "src/img/6_seis-copas.jpg";
+        break;
+      case 7:
+        imagen.src = "src/img/7_siete-copas.jpg";
+        break;
+      case 10:
+        imagen.src = "src/img/10_sota-copas.jpg";
+        break;
+      case 11:
+        imagen.src = "src/img/11_caballo-copas.jpg";
+        break;
+      case 12:
+        imagen.src = "src/img/12_rey-copas.jpg";
+        break;
+    }
+  }
 };
 
 /* --------------------------------------------FUNCION EVENTOS xra lso BOTONES--------------------- */
@@ -32,11 +74,6 @@ function eventos() {
   const botonEventos = document.getElementById("dame-carta");
 
   if (botonEventos) {
-    /*botonEventos.addEventListener("click", dameCarta);*/
-    /* comprobamos que todo funcione correctamente */
-    botonEventos.addEventListener("click", () => {
-      const valor = dameCarta();
-      console.log(valor);
-    });
+    botonEventos.addEventListener("click", dameCarta);
   }
 }
